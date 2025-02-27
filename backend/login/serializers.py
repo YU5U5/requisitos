cdd2ce1e7140ee969f8df246f2523727ea95012f
YI5U5 <137338018+YU5U5@users.noreply.github.com>
2025-02-27 17:36:33 -0500

    Delete backend directory

--- backend/login/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-class LoginSerializer(serializers.Serializer):
-    correo_electronico = serializers.EmailField()
-    password = serializers.CharField(write_only=True)
