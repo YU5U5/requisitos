6b659705ca5e990372085a2b8a5e6ca9789c3aca
YU5U5 <samuelosoriogaspar@gmail.com>
2025-02-27 17:20:52 -0500

    3 commit

--- backend/modificacion_datos/urls.py
+++ backend/modificacion_datos/urls.py
@@ -3,6 +3,5 @@ from .views import RegistroUsuarioView, ActualizarUsuarioView
 
 
 urlpatterns = [
-    path('', RegistroUsuarioView.as_view(), name='registro'),  # Ruta para registrar un usuario
     path('actualizar/<int:usuario_id>/', ActualizarUsuarioView.as_view(), name='actualizar_usuario'),  # Ruta para actualizar un usuario
 ]
