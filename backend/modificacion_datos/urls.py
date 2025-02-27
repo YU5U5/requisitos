2d5ec2d714b3400d29e1338625d8d3483b75d854
juanda321 <juandavidaguirre455@gmail.com>
2025-02-27 17:20:17 -0500

    commit 4

--- backend/modificacion_datos/urls.py
+++ backend/modificacion_datos/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import RegistroUsuarioView, ActualizarUsuarioView
+from .views import ActualizarUsuarioView
 
 
 urlpatterns = [
-    path('', RegistroUsuarioView.as_view(), name='registro'),  # Ruta para registrar un usuario
+    
     path('actualizar/<int:usuario_id>/', ActualizarUsuarioView.as_view(), name='actualizar_usuario'),  # Ruta para actualizar un usuario
 ]
