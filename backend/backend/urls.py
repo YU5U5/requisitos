c75dc9b0387af6ecbfb7fb7c334f9ddfa6fd5ee6
YU5U5 <samuelosoriogaspar@gmail.com>
2025-02-27 16:57:33 -0500

    papi o se commit

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registro/', include('registro.urls')),
+    path('login/', include('login.urls')),
 ]
--- backend/login/urls.py
+++ backend/login/urls.py
@@ -3,6 +3,6 @@ from .views import LoginView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),  # Ruta para el login
+    path('', LoginView.as_view(), name='login'),  # Ruta para el login
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Para refrescar el token
 ]
